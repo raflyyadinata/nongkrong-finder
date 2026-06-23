daad7cb0ff8fa8414d0217c73097648f88684d1e
unknown <gianluigi786@gmail.com>
2026-06-23 11:20:02 +0700

    Perbaikan dashboard statistik

--- src/main/java/com/nongkrongfinder/controller/AuthController.java
+++ src/main/java/com/nongkrongfinder/controller/AuthController.java
@@ -1,10 +1,17 @@
 package com.nongkrongfinder.controller;
 
 import com.nongkrongfinder.entity.User;
+import com.nongkrongfinder.repository.EventRepository;
+import com.nongkrongfinder.repository.FavoritRepository;
+import com.nongkrongfinder.repository.ReviewRepository;
+import com.nongkrongfinder.repository.TempatRepository;
 import com.nongkrongfinder.service.UserService;
+
 import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -12,6 +19,18 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TempatRepository tempatRepository;
+
+    @Autowired
+    private FavoritRepository favoritRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     @GetMapping("/register")
     public String registerPage() {
@@ -51,7 +70,28 @@ public class AuthController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+
+        model.addAttribute(
+                "jumlahTempat",
+                tempatRepository.count()
+        );
+
+        model.addAttribute(
+                "jumlahFavorit",
+                favoritRepository.count()
+        );
+
+        model.addAttribute(
+                "jumlahEvent",
+                eventRepository.count()
+        );
+
+        model.addAttribute(
+                "jumlahReview",
+                reviewRepository.count()
+        );
+
         return "dashboard";
     }
 
